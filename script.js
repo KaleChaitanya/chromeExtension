ee7c11e6e83b05f90c86521980de6e66a4c6128b
Chaitanya Kale <kale.chaitanya0304@gmail.com>
2023-09-28 10:29:05 +0530

    fix save tab button issue

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 const deleteBtn = document.querySelector("#delete-btn");
+const tabBtn = document.querySelector("#tab-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
     render(myLeads);
@@ -37,6 +37,14 @@ function deleteLeads() {
     myLeads = [];
     render(myLeads);
 }
+function saveTab() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+}
 
 inputBtn.addEventListener("click", saveLeads);
 deleteBtn.addEventListener("dblclick", deleteLeads);
+tabBtn.addEventListener("click", saveTab);
